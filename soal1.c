6211131bd87764960ad1f01ff6ded0c9b7faef81
IRONDHIF <abdan.nadhif@gmail.com>
2026-03-10 09:11:52 +0700

    Implement triangle type classification in soal1.c

---
+++ soal1.c
@@ -0,0 +1,38 @@
+int main() {
+    int a, b, c;
+    int max;
+
+    printf("Masukkan sisi segitiga: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("Bukan segitiga\n");
+        return 0;
+    }
+
+    max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+
+    if (a + b + c - max <= max) {
+        printf("Bukan segitiga\n");
+        return 0;
+    }else if (a == b && b == c) {
+        printf("Segitiga Sama Sisi\n");
+    }else if (a == b || b == c || a == c) {
+        printf("Segitiga Sama Kaki\n");
+    }else if ((a*a + b*b == c*c) || 
+             (a*a + c*c == b*b) || 
+             (b*b + c*c == a*a)) {
+        printf("Segitiga Siku-Siku\n");
+    }
+    else {
+        printf("Segitiga Bebas\n");
+    }
+
+    
+}
